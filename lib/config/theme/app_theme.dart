44f77e2f84f3d0a4f5f11c9707baba04814a7368
Victor Velazquez <victorvelazqueza@gmail.com>
2025-01-29 17:05:15 -0300

    feat: agregar colorSeedLight al tema de la aplicación para mejorar la personalización del diseño

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -4,6 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 const scaffoldBackgroundColor = Colors.white;
 final backgroundColor = Colors.grey[300];
 const colorSeed = Color.fromRGBO(20, 25, 45, 1);
+const colorSeedLight = Color.fromRGBO(70, 85, 130, 1);
 const editButtonColor = Color.fromRGBO(255, 170, 0, 1);
 const deleteButtonColor = Color.fromRGBO(200, 30, 30, 1);
 final cancelButtonColor = Colors.grey[600];
