54f8bcdf926a69c0e9d9c68655e75701d3957f5e
Victor Velazquez <victorvelazqueza@gmail.com>
2025-02-01 17:59:56 -0300

    feat: simplificar la clase Line eliminando propiedades innecesarias

--- lib/features/shipment/domain/entities/line.dart
+++ lib/features/shipment/domain/entities/line.dart
@@ -1,36 +1,8 @@
-import '../../../shared/domain/entities/ad_entity_id.dart';
-
 class Line {
     int? id;
-    String? uid;
-    AdEntityId adClientId;
-    AdEntityId adOrgId;
-    bool? isActive;
-    DateTime created;
-    AdEntityId createdBy;
-    DateTime updated;
-    AdEntityId updatedBy;
-    AdEntityId mLocatorId;
-    AdEntityId mProductId;
     int? movementQty;
-    int? line;
-    AdEntityId cOrderLineId;
-    AdEntityId cUomId;
-    bool? isInvoiced;
-    AdEntityId mAttributeSetInstanceId;
-    bool? isDescription;
-    double? confirmedQty;
-    double? pickedQty;
-    double? scrappedQty;
-    double? targetQty;
-    int? refInOutLineId;
-    bool? processed;
-    int? qtyEntered;
-    bool? isAutoProduce;
     String? upc;
     String? productName;
-    String? sku;
-    String? modelName;
     String? verifiedStatus;
     int? scanningQty;
 
@@ -38,137 +10,33 @@ class Line {
 
     Line({
         this.id,
-        this.uid,
-        required this.adClientId,
-        required this.adOrgId,
-        this.isActive,
-        required this.created,
-        required this.createdBy,
-        required this.updated,
-        required this.updatedBy,
-        required this.mLocatorId,
-        required this.mProductId,
         this.movementQty,
-        this.line,
-        required this.cOrderLineId,
-        required this.cUomId,
-        this.isInvoiced,
-        required this.mAttributeSetInstanceId,
-        this.isDescription,
-        this.confirmedQty,
-        this.pickedQty,
-        this.scrappedQty,
-        this.targetQty,
-        this.refInOutLineId,
-        this.processed,
-        this.qtyEntered,
-        this.isAutoProduce,
         this.upc,
         this.productName,
-        this.sku,
-        this.modelName,
         this.verifiedStatus,
         this.scanningQty,
     });
 
     factory Line.jsonToEntity(Map<String, dynamic> json) => Line(
         id: json["id"],
-        uid: json["uid"],
-        adClientId: AdEntityId.jsonToEntity(json["AD_Client_ID"]),
-        adOrgId: AdEntityId.jsonToEntity(json["AD_Org_ID"]),
-        isActive: json["IsActive"],
-        created: DateTime.parse(json["Created"]),
-        createdBy: AdEntityId.jsonToEntity(json["CreatedBy"]),
-        updated: DateTime.parse(json["Updated"]),
-        updatedBy: AdEntityId.jsonToEntity(json["UpdatedBy"]),
-        mLocatorId: AdEntityId.jsonToEntity(json["M_Locator_ID"]),
-        mProductId: AdEntityId.jsonToEntity(json["M_Product_ID"]),
         movementQty: json["MovementQty"],
-        line: json["Line"],
-        cOrderLineId: AdEntityId.jsonToEntity(json["C_OrderLine_ID"]),
-        cUomId: AdEntityId.jsonToEntity(json["C_UOM_ID"]),
-        isInvoiced: json["IsInvoiced"],
-        mAttributeSetInstanceId: AdEntityId.jsonToEntity(json["M_AttributeSetInstance_ID"]),
-        isDescription: json["IsDescription"],
-        confirmedQty: json["ConfirmedQty"],
-        pickedQty: json["PickedQty"],
-        scrappedQty: json["ScrappedQty"],
-        targetQty: json["TargetQty"],
-        refInOutLineId: json["Ref_InOutLine_ID"],
-        processed: json["Processed"],
-        qtyEntered: json["QtyEntered"],
-        isAutoProduce: json["IsAutoProduce"],
         upc: json["UPC"],
         productName: json["ProductName"],
-        sku: json["SKU"],
-        modelName: json["model-name"],
     );
 
     Line copyWith({
       int? id,
-      String? uid,
-      AdEntityId? adClientId,
-      AdEntityId? adOrgId,
-      bool? isActive,
-      DateTime? created,
-      AdEntityId? createdBy,
-      DateTime? updated,
-      AdEntityId? updatedBy,
-      AdEntityId? mLocatorId,
-      AdEntityId? mProductId,
       int? movementQty,
-      int? line,
-      AdEntityId? cOrderLineId,
-      AdEntityId? cUomId,
-      bool? isInvoiced,
-      AdEntityId? mAttributeSetInstanceId,
-      bool? isDescription,
-      double? confirmedQty,
-      double? pickedQty,
-      double? scrappedQty,
-      double? targetQty,
-      int? refInOutLineId,
-      bool? processed,
-      int? qtyEntered,
-      bool? isAutoProduce,
       String? upc,
       String? productName,
-      String? sku,
-      String? modelName,
       String? verifiedStatus,
       int? scanningQty,
     }) {
       return Line(
         id: id ?? this.id,
-        uid: uid ?? this.uid,
-        adClientId: adClientId ?? this.adClientId,
-        adOrgId: adOrgId ?? this.adOrgId,
-        isActive: isActive ?? this.isActive,
-        created: created ?? this.created,
-        createdBy: createdBy ?? this.createdBy,
-        updated: updated ?? this.updated,
-        updatedBy: updatedBy ?? this.updatedBy,
-        mLocatorId: mLocatorId ?? this.mLocatorId,
-        mProductId: mProductId ?? this.mProductId,
         movementQty: movementQty ?? this.movementQty,
-        line: line ?? this.line,
-        cOrderLineId: cOrderLineId ?? this.cOrderLineId,
-        cUomId: cUomId ?? this.cUomId,
-        isInvoiced: isInvoiced ?? this.isInvoiced,
-        mAttributeSetInstanceId: mAttributeSetInstanceId ?? this.mAttributeSetInstanceId,
-        isDescription: isDescription ?? this.isDescription,
-        confirmedQty: confirmedQty ?? this.confirmedQty,
-        pickedQty: pickedQty ?? this.pickedQty,
-        scrappedQty: scrappedQty ?? this.scrappedQty,
-        targetQty: targetQty ?? this.targetQty,
-        refInOutLineId: refInOutLineId ?? this.refInOutLineId,
-        processed: processed ?? this.processed,
-        qtyEntered: qtyEntered ?? this.qtyEntered,
-        isAutoProduce: isAutoProduce ?? this.isAutoProduce,
         upc: upc ?? this.upc,
         productName: productName ?? this.productName,
-        sku: sku ?? this.sku,
-        modelName: modelName ?? this.modelName,
         verifiedStatus: verifiedStatus ?? this.verifiedStatus,
         scanningQty: scanningQty ?? this.scanningQty,
       );
--- lib/features/shipment/domain/entities/shipment.dart
+++ lib/features/shipment/domain/entities/shipment.dart
@@ -3,290 +3,70 @@ import 'line.dart';
 
 class Shipment {
     int? id;
-    String? uid;
-    String? description;
-    AdEntityId movementType;
     DateTime movementDate;
-    bool? processed;
-    AdEntityId adClientId;
     AdEntityId adOrgId;
-    bool? isActive;
-    DateTime created;
-    AdEntityId createdBy;
-    DateTime updated;
-    AdEntityId updatedBy;
     bool? isSoTrx;
     String? documentNo;
-    AdEntityId cDocTypeId;
-    bool? isPrinted;
-    DateTime dateAcct;
     AdEntityId cBPartnerId;
-    AdEntityId cBPartnerLocationId;
-    AdEntityId adUserId;
     AdEntityId mWarehouseId;
-    AdEntityId deliveryRule;
-    AdEntityId freightCostRule;
-    int? freightAmt;
-    AdEntityId deliveryViaRule;
-    int? chargeAmt;
-    AdEntityId priorityRule;
     AdEntityId cOrderId;
     DateTime dateOrdered;
     AdEntityId docStatus;
-    bool? sendEMail;
-    AdEntityId salesRepId;
-    int? noPackages;
-    bool? isInTransit;
-    bool? isApproved;
-    bool? isInDispute;
-    double? volume;
-    double? weight;
-    bool? isDropShip;
-    double? processedOn;
-    bool? isAlternateReturnAddress;
-    String? moliVehicleType;
-    String? drivername;
-    String? moliTransportDirection;
-    String? moliRucTransportista;
-    String? driverid;
-    String? moliDistancia;
-    String? vehicle;
-    String? vehicleid;
-    DateTime moliEndTransfer;
-    DateTime moliStartTransfer;
-    String? moliTransportista;
-    String? modelName;
     List<Line> lines;
 
 
     Shipment({
         this.id,
-        this.uid,
-        this.description,
-        required this.movementType,
         required this.movementDate,
-        this.processed,
-        required this.adClientId,
         required this.adOrgId,
-        this.isActive,
-        required this.created,
-        required this.createdBy,
-        required this.updated,
-        required this.updatedBy,
         this.isSoTrx,
         this.documentNo,
-        required this.cDocTypeId,
-        this.isPrinted,
-        required this.dateAcct,
         required this.cBPartnerId,
-        required this.cBPartnerLocationId,
-        required this.adUserId,
         required this.mWarehouseId,
-        required this.deliveryRule,
-        required this.freightCostRule,
-        this.freightAmt,
-        required this.deliveryViaRule,
-        this.chargeAmt,
-        required this.priorityRule,
         required this.cOrderId,
         required this.dateOrdered,
         required this.docStatus,
-        this.sendEMail,
-        required this.salesRepId,
-        this.noPackages,
-        this.isInTransit,
-        this.isApproved,
-        this.isInDispute,
-        this.volume,
-        this.weight,
-        this.isDropShip,
-        this.processedOn,
-        this.isAlternateReturnAddress,
-        this.moliVehicleType,
-        this.drivername,
-        this.moliTransportDirection,
-        this.moliRucTransportista,
-        this.driverid,
-        this.moliDistancia,
-        this.vehicle,
-        this.vehicleid,
-        required this.moliEndTransfer,
-        required this.moliStartTransfer,
-        this.moliTransportista,
-        this.modelName,
         this.lines = const [],
     });
 
     factory Shipment.jsonToEntity(Map<String, dynamic> json) => Shipment(
         id: json["id"],
-        uid: json["uid"],
-        description: json["Description"],
-        movementType: AdEntityId.jsonToEntity(json["MovementType"]),
         movementDate: DateTime.parse(json["MovementDate"]),
-        processed: json["Processed"],
-        adClientId: AdEntityId.jsonToEntity(json["AD_Client_ID"]),
         adOrgId: AdEntityId.jsonToEntity(json["AD_Org_ID"]),
-        isActive: json["IsActive"],
-        created: DateTime.parse(json["Created"]),
-        createdBy: AdEntityId.jsonToEntity(json["CreatedBy"]),
-        updated: DateTime.parse(json["Updated"]),
-        updatedBy: AdEntityId.jsonToEntity(json["UpdatedBy"]),
         isSoTrx: json["IsSOTrx"],
         documentNo: json["DocumentNo"],
-        cDocTypeId: AdEntityId.jsonToEntity(json["C_DocType_ID"]),
-        isPrinted: json["IsPrinted"],
-        dateAcct: DateTime.parse(json["DateAcct"]),
         cBPartnerId: AdEntityId.jsonToEntity(json["C_BPartner_ID"]),
-        cBPartnerLocationId: AdEntityId.jsonToEntity(json["C_BPartner_Location_ID"]),
-        adUserId: AdEntityId.jsonToEntity(json["AD_User_ID"]),
         mWarehouseId: AdEntityId.jsonToEntity(json["M_Warehouse_ID"]),
-        deliveryRule: AdEntityId.jsonToEntity(json["DeliveryRule"]),
-        freightCostRule: AdEntityId.jsonToEntity(json["FreightCostRule"]),
-        freightAmt: json["FreightAmt"],
-        deliveryViaRule: AdEntityId.jsonToEntity(json["DeliveryViaRule"]),
-        chargeAmt: json["ChargeAmt"],
-        priorityRule: AdEntityId.jsonToEntity(json["PriorityRule"]),
         cOrderId: AdEntityId.jsonToEntity(json["C_Order_ID"]),
         dateOrdered: DateTime.parse(json["DateOrdered"]),
         docStatus: AdEntityId.jsonToEntity(json["DocStatus"]),
-        sendEMail: json["SendEMail"],
-        salesRepId: AdEntityId.jsonToEntity(json["SalesRep_ID"]),
-        noPackages: json["NoPackages"],
-        isInTransit: json["IsInTransit"],
-        isApproved: json["IsApproved"],
-        isInDispute: json["IsInDispute"],
-        volume: json["Volume"],
-        weight: json["Weight"],
-        isDropShip: json["IsDropShip"],
-        processedOn: json["ProcessedOn"].toDouble(),
-        isAlternateReturnAddress: json["IsAlternateReturnAddress"],
-        moliVehicleType: json["MOLI_VehicleType"],
-        drivername: json["DRIVERNAME"],
-        moliTransportDirection: json["MOLI_TransportDirection"],
-        moliRucTransportista: json["MOLI_RucTransportista"],
-        driverid: json["DRIVERID"],
-        moliDistancia: json["MOLI_Distancia"],
-        vehicle: json["VEHICLE"],
-        vehicleid: json["VEHICLEID"],
-        moliEndTransfer: DateTime.parse(json["MOLI_EndTransfer"]),
-        moliStartTransfer: DateTime.parse(json["MOLI_StartTransfer"]),
-        moliTransportista: json["MOLI_Transportista"],
-        modelName: json["model-name"],
         lines: List<Line>.from(json["m_inoutline"].map((x) => Line.jsonToEntity(x))),
     );
 
     Shipment copyWith({
       int? id,
-      String? uid,
-      String? description,
-      AdEntityId? movementType,
       DateTime? movementDate,
-      bool? processed,
-      AdEntityId? adClientId,
       AdEntityId? adOrgId,
-      bool? isActive,
-      DateTime? created,
-      AdEntityId? createdBy,
-      DateTime? updated,
-      AdEntityId? updatedBy,
       bool? isSoTrx,
       String? documentNo,
-      AdEntityId? cDocTypeId,
-      bool? isPrinted,
-      DateTime? dateAcct,
       AdEntityId? cBPartnerId,
-      AdEntityId? cBPartnerLocationId,
-      AdEntityId? adUserId,
       AdEntityId? mWarehouseId,
-      AdEntityId? deliveryRule,
-      AdEntityId? freightCostRule,
-      int? freightAmt,
-      AdEntityId? deliveryViaRule,
-      int? chargeAmt,
-      AdEntityId? priorityRule,
       AdEntityId? cOrderId,
       DateTime? dateOrdered,
       AdEntityId? docStatus,
-      bool? sendEMail,
-      AdEntityId? salesRepId,
-      int? noPackages,
-      bool? isInTransit,
-      bool? isApproved,
-      bool? isInDispute,
-      double? volume,
-      double? weight,
-      bool? isDropShip,
-      double? processedOn,
-      bool? isAlternateReturnAddress,
-      String? moliVehicleType,
-      String? drivername,
-      String? moliTransportDirection,
-      String? moliRucTransportista,
-      String? driverid,
-      String? moliDistancia,
-      String? vehicle,
-      String? vehicleid,
-      DateTime? moliEndTransfer,
-      DateTime? moliStartTransfer,
-      String? moliTransportista,
-      String? modelName,
       List<Line>? lines,
     }) {
       return Shipment(
         id: id ?? this.id,
-        uid: uid ?? this.uid,
-        description: description ?? this.description,
-        movementType: movementType ?? this.movementType,
         movementDate: movementDate ?? this.movementDate,
-        processed: processed ?? this.processed,
-        adClientId: adClientId ?? this.adClientId,
         adOrgId: adOrgId ?? this.adOrgId,
-        isActive: isActive ?? this.isActive,
-        created: created ?? this.created,
-        createdBy: createdBy ?? this.createdBy,
-        updated: updated ?? this.updated,
-        updatedBy: updatedBy ?? this.updatedBy,
         isSoTrx: isSoTrx ?? this.isSoTrx,
         documentNo: documentNo ?? this.documentNo,
-        cDocTypeId: cDocTypeId ?? this.cDocTypeId,
-        isPrinted: isPrinted ?? this.isPrinted,
-        dateAcct: dateAcct ?? this.dateAcct,
         cBPartnerId: cBPartnerId ?? this.cBPartnerId,
-        cBPartnerLocationId: cBPartnerLocationId ?? this.cBPartnerLocationId,
-        adUserId: adUserId ?? this.adUserId,
         mWarehouseId: mWarehouseId ?? this.mWarehouseId,
-        deliveryRule: deliveryRule ?? this.deliveryRule,
-        freightCostRule: freightCostRule ?? this.freightCostRule,
-        freightAmt: freightAmt ?? this.freightAmt,
-        deliveryViaRule: deliveryViaRule ?? this.deliveryViaRule,
-        chargeAmt: chargeAmt ?? this.chargeAmt,
-        priorityRule: priorityRule ?? this.priorityRule,
         cOrderId: cOrderId ?? this.cOrderId,
         dateOrdered: dateOrdered ?? this.dateOrdered,
         docStatus: docStatus ?? this.docStatus,
-        sendEMail: sendEMail ?? this.sendEMail,
-        salesRepId: salesRepId ?? this.salesRepId,
-        noPackages: noPackages ?? this.noPackages,
-        isInTransit: isInTransit ?? this.isInTransit,
-        isApproved: isApproved ?? this.isApproved,
-        isInDispute: isInDispute ?? this.isInDispute,
-        volume: volume ?? this.volume,
-        weight: weight ?? this.weight,
-        isDropShip: isDropShip ?? this.isDropShip,
-        processedOn: processedOn ?? this.processedOn,
-        isAlternateReturnAddress: isAlternateReturnAddress ?? this.isAlternateReturnAddress,
-        moliVehicleType: moliVehicleType ?? this.moliVehicleType,
-        drivername: drivername ?? this.drivername,
-        moliTransportDirection: moliTransportDirection ?? this.moliTransportDirection,
-        moliRucTransportista: moliRucTransportista ?? this.moliRucTransportista,
-        driverid: driverid ?? this.driverid,
-        moliDistancia: moliDistancia ?? this.moliDistancia,
-        vehicle: vehicle ?? this.vehicle,
-        vehicleid: vehicleid ?? this.vehicleid,
-        moliEndTransfer: moliEndTransfer ?? this.moliEndTransfer,
-        moliStartTransfer: moliStartTransfer ?? this.moliStartTransfer,
-        moliTransportista: moliTransportista ?? this.moliTransportista,
-        modelName: modelName ?? this.modelName,
         lines: lines ?? this.lines,
       );
     }
