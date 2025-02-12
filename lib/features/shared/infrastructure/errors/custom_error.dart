c7adb5966aa94442839711e63c5fea3a69677bec
Victor Velazquez <victorvelazqueza@gmail.com>
2025-02-12 07:55:48 -0300

    feat: mejorar el manejo de excepciones de autenticación en CustomErrorDioException

--- lib/features/shared/infrastructure/errors/custom_error.dart
+++ lib/features/shared/infrastructure/errors/custom_error.dart
@@ -13,18 +13,13 @@ class CustomErrorDioException implements Exception {
           'No se pudo conectar a internet. Conéctate para continuar.');
     }
     if (e.response?.statusCode == 401) {
-      if (e.response?.data['title'] == 'Authenticate error') {
+      if (e.response?.data is! String && e.response?.data['title'] == 'Authenticate error') {
         throw Exception(e.response?.data['message'] ??
             'Credenciales incorrectas. Vuelve a intentarlo.');
       } else if (e.response?.statusMessage == 'Unauthorized') {
-        if (authNotifier == null) {
-          print('AUTH NULLLLLLLLLLLLLLL');
-        } else {
-          print('AUTH OKKKKKKKKKKKKKKKK');
-          // authNotifier?.checkAuthStatus() ?? () {};
-        }
+          authNotifier?.checkAuthStatus() ?? () {};
         throw Exception(
-            'Sesión caducada. Por favor, inicia sesión nuevamente.');
+            'Sesión caducada. Por favor, intente nuevamente.');
       } else {
         throw Exception(e.response?.data['message'] ??
             'Credenciales incorrectas. Vuelve a intentarlo.');
